3c1b65de42fa387a85581b167af77f98f739d12a
Zerthick <zerthick@gmail.com>
2017-05-25 23:08:01 -0400

    Add copyright, and readme.

--- src/main/java/io/github/zerthick/nosleep/NoSleep.java
+++ src/main/java/io/github/zerthick/nosleep/NoSleep.java
@@ -1,3 +1,22 @@
+/*
+ * Copyright (C) 2017  Zerthick
+ *
+ * This file is part of NoSleep.
+ *
+ * NoSleep is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * NoSleep is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with NoSleep.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package io.github.zerthick.nosleep;
 
 import com.google.inject.Inject;
@@ -8,13 +27,12 @@ import org.spongepowered.api.event.filter.Getter;
 import org.spongepowered.api.event.game.state.GameStartedServerEvent;
 import org.spongepowered.api.event.network.ClientConnectionEvent;
 import org.spongepowered.api.plugin.Plugin;
-import org.spongepowered.api.text.Text;
-import org.spongepowered.api.text.format.TextColors;
-import org.spongepowered.api.text.format.TextStyles;
+import org.spongepowered.api.plugin.PluginContainer;
 
 @Plugin(
         id = "nosleep",
         name = "NoSleep",
+        version = "1.0.0",
         description = "A simple plugin to prevent sleeping through the night.",
         authors = {
                 "Zerthick"
@@ -25,20 +43,30 @@ public class NoSleep {
     @Inject
     private Logger logger;
 
+    @Inject
+    private PluginContainer instance;
 
     @Listener
     public void onServerStart(GameStartedServerEvent event) {
-        // Hey! The server has started!
-        this.logger.info("Hello world!");
-        // Try loading some configuration settings for a welcome message to players
-        // when they join!
+        // Log Start Up to Console
+        getLogger().info(
+                instance.getName() + " version " + instance.getVersion().orElse("")
+                        + " enabled!");
     }
 
 
     @Listener
     public void onPlayerJoin(ClientConnectionEvent.Join event, @Getter("getTargetEntity") Player player) {
-        // The text message could be configurable, check the docs on how to do so!
-        player.sendMessage(Text.of(TextColors.AQUA, TextStyles.BOLD, "Hi " + player.getName()));
+
+        // Disable Sleeping for the Player
+        player.setSleepingIgnored(true);
     }
 
+    public Logger getLogger() {
+        return logger;
+    }
+
+    public PluginContainer getInstance() {
+        return instance;
+    }
 }
